4010ee81993bfd7823f0bc808c55ab1467515f1d
sap5699 <sap5699@psu.edu>
2020-04-26 14:06:41 -0400

    my team is working real hard on that video

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template
-println("HELLO STEEEVEN");
-def create_app():
-	app = Flask(__name__)
-	@app.route('/')
-	def home():
-		return render_template('index.html');
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html');
+
+	
