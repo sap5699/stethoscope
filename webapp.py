81828a10612b0eeb325f02ef587108449b5d59d1
sap5699 <sap5699@psu.edu>
2020-04-26 13:23:24 -0400

    more heroku stuff

--- webapp.py
+++ webapp.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+def create_app():
+	return app
